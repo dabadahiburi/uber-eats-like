5c238ab38d9ef6605eb7bc25658435f9328affd5
dabadahiburi <yuuki11250310495@gmail.com>
2021-05-11 08:26:43 +0900

    add api reducer

--- frontend/src/apis/foods.js
+++ frontend/src/apis/foods.js
@@ -4,7 +4,7 @@ import { foodsIndex } from '../urls/index'
 export const fetchFoods = (restaurantId) => {
   return axios.get(foodsIndex(restaurantId))
     .then(res => {
-    return res.date
+    return res.data
     })
     .catch((e)=>console.error(e))
 }
